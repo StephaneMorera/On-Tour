e03449d5332339f6ac10814947ea89bfe4867520
Stephane Morera <stephanemorera@Stephanes-MacBook-Pro.local>
2023-02-08 16:54:09 -0500

    testing fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./styles/_global.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere nam hic architecto quae ipsam tenetur repellat illum alias doloremque fuga quisquam, nobis tempora, ullam, eligendi sint fugit tempore perferendis non.</h1>
+    </div>
+  );
 }
 
 export default App;
