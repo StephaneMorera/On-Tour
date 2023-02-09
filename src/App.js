4d6d209fef38c0f296150352256824a69e2d9c1b
Stephane Morera <stephanemorera@Stephanes-MacBook-Pro.local>
2023-02-08 19:52:25 -0500

    set up routes. added a few styles folders.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./styles/_global.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage"
+import EventDetailsPage from "./pages/EventDetailsPage/EventDetailsPage"
+import ProfilePage from "./pages/ProfilePage/ProfilePage"
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="events" element={<EventDetailsPage />}/>
+      <Route path="profile" element={<ProfilePage />}/>
+      <Route path="*" element={<NotFoundPage />}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
