45e424ba693b93aa9c990fe8b725b48c3c19ab65
Stephane Morera <stephanemorera@Stephanes-MacBook-Pro.local>
2023-02-11 01:45:26 -0500

    started events page. created component to display event cards on the page.

--- src/pages/EventDetailsPage/EventDetailsPage.js
+++ src/pages/EventDetailsPage/EventDetailsPage.js
@@ -1,5 +1,15 @@
 import "./EventDetailsPage.scss";
 
-const EventDetailsPage = () => {};
+const EventDetailsPage = () => {
+    return (
+        <main className="events">
+            <form className="events__search">
+                <h1 className="events__search-header">Search upcoming events for any location</h1>
+                <label className="events__label">See who's about to take the stage</label>
+                <input className="events__input" type="text" placeholder="Search events"  />
+            </form>
+        </main>
+    )
+};
 
 export default EventDetailsPage;
