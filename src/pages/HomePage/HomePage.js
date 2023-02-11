88138f4add472b7a64e20d0f8f93b834a64040a9
Stephane Morera <stephanemorera@Stephanes-MacBook-Pro.local>
2023-02-10 21:44:50 -0500

    fixed issue with large file. created new branch becaue the original homepage branch was 3 commits ahead of the commit where the large file was added

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,20 @@
 import "./HomePage.scss";
+import video from "../../video/on-tour.mp4"
 
-const HomePage = () => {};
+const HomePage = () => {
+    return (
+        <main className="home">
+            <section className="home__banner">
+                <article className="home__text-wrapper">
+                    <h1 className="home__header">The Next Show's About To Start!</h1>
+                    <p className="home__description">Your favorite musician is on tour and we dont want you to miss the show! Search for any and all upcoming shows by location to start planning your next adventure.</p>
+                </article>
+            </section>
+            <div className="home__video-container">
+                <video className="home__video" src={video} type="video/mp4" autoPlay muted width="70%" height="30%"></video>
+            </div>
+        </main>
+    )
+};
 
 export default HomePage;
