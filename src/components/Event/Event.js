342477108afbd044883f88a9b15ee8d999eb44eb
Stephane Morera <stephanemorera@Stephanes-MacBook-Pro.local>
2023-02-12 03:13:48 -0500

    search function to filter through events is working.

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -3,13 +3,12 @@ import "./Event.scss";
 const Event = ({ event }) => {
   return (
     <article key={event.id} className="card">
-      {/* <div className="event__image-container"> */}
       <img className="card__image" src={event.image} alt="show poster" />
-      {/* </div> */}
       <h2 className="card__title">{event.name}</h2>
-      <span className="card__city">{event.city}, {event.state}</span>
+      <span className="card__city">
+        {event.city}, {event.state}
+      </span>
       <span className="card__date">{event.date}</span>
-    
     </article>
   );
 };
--- src/pages/EventDetailsPage/EventDetailsPage.js
+++ src/pages/EventDetailsPage/EventDetailsPage.js
@@ -5,58 +5,57 @@ import Event from "../../components/Event/Event";
 import { Link } from "react-router-dom";
 
 const EventDetailsPage = () => {
-    const [card, setCard] = useState([])
-    const [query, setQuery] = useState("")
-    
+  const [card, setCard] = useState([]);
+  const [search, setSearch] = useState("");
+  console.log(search);
 
+  useEffect(() => {
+    const getEvent = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:8080/events");
+        setCard(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getEvent();
+  }, []);
 
-    useEffect(() => {
-      const getEvent = async () => {
-        try {
-          const { data } = await axios.get("http://localhost:8080/events");
-          console.log(data);
-          setCard(data)
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      getEvent();
-    }, []);
-
-    // useEffect(() => {
-    //     const getQuery = async () => {
-    //         try {
-    //             const { data } = await axios.get(`http://localhost:8080/events?q=${query}`);
-    //                 setSearchCard(data)
-    //         } catch (error) {
-    //             console.log(error.message)
-    //         }
-    //     };
-    //     getQuery()   
-    //   }, [query])
-
-    let events = card.map((event) => {
-        return (
-            <>
-            <Event key={event.id} event={event} />
-            </>
-        )
+  let events = card
+    .filter((value) => {
+      if (search === "") {
+        return value;
+      } else if (value.city.toLowerCase().includes(search.toLowerCase())) {
+        return value;
+      }
     })
+    .map((event) => {
+      return (
+        <>
+          <Event key={event.id} event={event} />
+        </>
+      );
+    });
 
-
-    return (
-        <main events={events} className="events">
-            <form className="events__search">
-                <h1 className="events__search-header">Search upcoming events for any location</h1>
-                <label className="events__label">See who's about to take the stage</label>
-                <input className="events__input" type="text" placeholder="Search events" onChange={(e) => setQuery(e.target.value)} />
-                <button className="events__button" >Search</button>
-            </form>
-            <section className="events__container">
-                {events}
-            </section>
-        </main>
-    )
+  return (
+    <main events={events} className="events">
+      <form className="events__search">
+        <h1 className="events__search-header">
+          Search upcoming events for any location
+        </h1>
+        <label className="events__label">
+          See who's about to take the stage
+        </label>
+        <input
+          className="events__input"
+          type="text"
+          placeholder="Search events"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <section className="events__container">{events}</section>
+    </main>
+  );
 };
 
 export default EventDetailsPage;
